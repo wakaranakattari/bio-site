c772ead1318a36c4d014d2d4b56fba102ac6f2a5
Nikita <wakaranakattari@gmail.com>
2026-05-17 18:43:06 -0700

    feat: all buttons style has been changed & add button theme switch

--- src/bio_site/ui/components/header.cljs
+++ src/bio_site/ui/components/header.cljs
@@ -1,38 +1,48 @@
-;; @file    components/header.cljs
+;; @file    <components/header.cljs>
 ;; @author  <wakaranakattari@gmail.com>
 ;; @info    <header component>
-;; @version 1.3
+;; @version <1.4>
 
-;; @secstart->@secname <ns>
-(ns bio-site.ui.components.header)
-;; @secend->@secname   <ns>
+;; @secstart->@secname <nsrc>
+(ns bio-site.ui.components.header
+  (:require [bio-site.ui.components.theme-toggle :refer [theme-toggle]]
+            [bio-site.utils.theme :as theme]))
+
+;; @secend->@secname   <nsrc>
 
 ;; @secstart->@secname <headerrouting>
   ;; @funcinfo <header routing to highlight the active page>
 (defn header []
   (let [path (.-pathname js/location)]
     (fn []
+
+      (let [current-theme @theme/theme-state]
+        (theme/apply-theme current-theme))
+
       [:header.header
 
        ;; @secstart->@secname <navigation> :: @secinfo <navigation section & basic routing header>
        ;; @info <routing in "/" home page, if active => highlight>
-       [:a.nav-item {:href "/" :class
-                     (when (= path "/") "active")} "home"]
+       [:a.nav-item  {:href "/" :class
+                      (when (= path "/") "active")} "home"]
 
       ;; @info <routing in "/about" about page, if active => highlight>
-       [:a.nav-item {:href "/about" :class
-                     (when (= path "/about") "active")} "about"]
+       [:a.nav-item  {:href "/about" :class
+                      (when (= path "/about") "active")} "about"]
 
        ;; @info <routing in "/projects" projects page, if active => highlight>
-       [:a.nav-item {:href "/projects" :class
-                     (when (= path "/projects") "active")} "projects"]
+       [:a.nav-item  {:href "/projects" :class
+                      (when (= path "/projects") "active")} "projects"]
 
        ;; @info <routing in "/writing" writing page, if active => highlight>
-       [:a.nav-item {:href "/writing" :class
-                     (when (= path "/writing") "active")} "writing"]
+       [:a.nav-item  {:href "/writing" :class
+                      (when (= path "/writing") "active")} "writing"]
 
        ;; @info <routing in "/contacts" contacts page, if active => highlight>
-       [:a.nav-item {:href "/contacts" :class
-                     (when (= path "/contacts") "active")} "contacts"]])))
+       [:a.nav-item  {:href "/contacts" :class
+                      (when (= path "/contacts") "active")} "contacts"]
+
+       ;; @info <theme toggle button>
+       [theme-toggle]])))
       ;; @secend->@secname    <navigation>
 ;; @secend->@secname   <headerrouting>
