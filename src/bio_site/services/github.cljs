1234ad5dde89878adf939a4df5e1de26d03766b0
Nikita <wakaranakattari@gmail.com>
2026-06-26 22:09:49 +0500

    refactor(github): optimized API requests and added statistics of repository languages ​​from github

--- src/bio_site/services/github.cljs
+++ src/bio_site/services/github.cljs
@@ -1,7 +1,7 @@
 ;; @file    <services/github.cljs>
 ;; @author  <wakaranakattari@gmail.com>
 ;; @info    <github fetch api for dynamic loading repos>
-;; @version <1.4>
+;; @version <1.5>
 
 ;; @secstart->@secname <ns>
 (ns bio-site.services.github)
@@ -12,19 +12,49 @@
 (defn fetch-repos!
   [on-success on-error]
 
-  ;; @info <getting fetch via api>
-  (-> (js/fetch "https://api.github.com/users/wakaranakattari/repos?sort=updated&per_page=100")
+  ;; @info <use AbortController for cleanup, but keep it simple for speed>
+  ;; @info <fetch with cache: force-cache for faster subsequent loads>
+  (-> (js/fetch "https://api.github.com/users/wakaranakattari/repos?sort=updated&per_page=100" 
+                #js {:cache "force-cache"})
       ;; @info <if getting fetch is successfully, trying parse to json>
       (.then #(.json %))
       ;; @info <if parse to json is successfully, convert to clj map and filter out forked repos>
       (.then (fn [data]
-               (let [repos (js->clj data :keywordize-keys true)
+               (let [repos    (js->clj data :keywordize-keys true)
                      filtered (filter #(not (:fork %)) repos)]
 
-                ;; @info <if convert to clj map etc is successfully>
-                 (on-success filtered))))
+                 ;; @info <fetch languages for each repo in parallel with Promise.all>
+                 (-> (js/Promise.all
+                      (clj->js
+                       (map (fn [repo]
+                              (-> (js/fetch (:languages_url repo) #js {:cache "force-cache"})
+                                  (.then #(.json %))
+                                  ;; @info <attach languages map to repo>
+                                  (.then (fn [langs]
+                                           (assoc repo :languages (js->clj langs))))))
+                            filtered)))
+                     ;; @info <on success: pass repos with languages to callback>
+                     (.then (fn [repos-with-langs]
+                              (on-success (js->clj repos-with-langs :keywordize-keys true))))))))
 
       ;; @info <if fetch via api || parse to json || convert to clj map is failed>
       (.catch (fn [err]
                 (on-error (.-message err))))))
 ;; @secend->@secname   <fetchrepos>
+
+;; @secstart->@secname <calcpercents>
+  ;; @funcinfo <calculate language percentages from raw bytes map>
+(defn calc-lang-percents
+  [languages]
+
+  ;; @info <sum all bytes>
+  (let [total (reduce + (vals languages))]
+    (if (zero? total)
+      []
+      ;; @info <map each language to name + percentage, sort descending>
+      (->> languages
+           (map (fn [[lang bytes]]
+                  {:lang    (name lang)
+                   :percent (/ (* bytes 100.0) total)}))
+           (sort-by :percent >)))))
+;; @secend->@secname   <calcpercents>
